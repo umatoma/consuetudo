bd8db16392d2739fc28e55e39204477e1f14e1a0
umatoma <umatomakun@gmail.com>
2020-03-06 21:44:50 +0900

    サインインページのデザイン調整

--- flutter/lib/page/sign_in_page.dart
+++ flutter/lib/page/sign_in_page.dart
@@ -1,6 +1,7 @@
 import 'package:consuetudo/model/AuthModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -8,16 +9,25 @@ class SignInPage extends StatelessWidget {
     var authModel = Provider.of<AuthModel>(context, listen: false);
     return Scaffold(
       body: Container(
-        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Consuetodo'),
-              RaisedButton(
+              Container(
+                child: Image(
+                  image: AssetImage('assets/images/app_icon.png'),
+                  width: 128,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text('Consuetodo',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              SizedBox(height: 64),
+              SignInButton(
+                Buttons.Google,
                 onPressed: () => authModel.signIn(),
-                child: Text('Sign in with Google'),
-                color: Colors.lightBlueAccent,
               ),
             ],
           ),
