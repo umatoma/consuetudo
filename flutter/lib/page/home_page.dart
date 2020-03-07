eb0295399ce1d55815c900bfa1f4d0b4cece7430
umatoma <umatomakun@gmail.com>
2020-03-07 17:20:00 +0900

    ホーム画面のデザイン調整

--- flutter/lib/page/home_page.dart
+++ flutter/lib/page/home_page.dart
@@ -2,10 +2,13 @@ import 'package:consuetudo/model/AuthModel.dart';
 import 'package:consuetudo/page/post_habit_page.dart';
 import 'package:consuetudo/page/view_habit_page.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatelessWidget {
+  final DateTime targetDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     final authModel = Provider.of<AuthModel>(context);
@@ -15,14 +18,20 @@ class HomePage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () { Navigator.pushNamed(context, PostHabitPage.routeName); },
+            onPressed: () {
+              Navigator.pushNamed(context, PostHabitPage.routeName);
+            },
           ),
           PopupMenuButton<VoidCallback>(
-            onSelected: (VoidCallback callback) { callback(); },
+            onSelected: (VoidCallback callback) {
+              callback();
+            },
             itemBuilder: (context) {
               return [
                 PopupMenuItem<VoidCallback>(
-                  value: () { authModel.signOut(); },
+                  value: () {
+                    authModel.signOut();
+                  },
                   child: Text('ログアウト'),
                 ),
               ];
@@ -30,12 +39,93 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: _HabitList(),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _Head(targetDate: targetDate),
+            Expanded(
+              child: _HabitList(targetDate: targetDate),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Head extends StatelessWidget {
+  static const upperHeight = 64.0;
+  static const lowerHeight = 48.0;
+  final DateTime targetDate;
+
+  const _Head({Key key, @required this.targetDate}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: upperHeight + lowerHeight,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: upperHeight + (lowerHeight / 2.0),
+            color: Colors.blue,
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: upperHeight,
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        '習慣を記録',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.arrow_forward_ios),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Card(
+              child: Container(
+                width: 192,
+                height: lowerHeight - 8.0, // Padding調整
+                child: Center(
+                  child: Text(DateFormat.MMMMd().format(targetDate)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
 class _HabitList extends StatelessWidget {
+  final DateTime targetDate;
+
+  const _HabitList({Key key, @required this.targetDate}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final authModel = Provider.of<AuthModel>(context);
@@ -53,20 +143,45 @@ class _HabitList extends StatelessWidget {
         }
         return ListView(
           children: snapshot.data.documents.map((DocumentSnapshot document) {
-            return ListTile(
-              onTap: () {
-                Navigator.pushNamed(
-                    context,
-                    ViewHabitPage.routeName,
-                    arguments: ViewHabitPageArguments(document),
-                );
-              },
-              title: Text(document['id']),
-              subtitle: Text(document['name']),
+            return Card(
+              child: ListTile(
+                onTap: () {
+                  // TODO: Recordの更新処理
+                },
+                leading: Icon(
+                  Icons.check,
+                  color: _isRecordedOn(targetDate, document)
+                      ? Colors.blue
+                      : Colors.grey,
+                ),
+                title: Text(document['name']),
+                trailing: IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      ViewHabitPage.routeName,
+                      arguments: ViewHabitPageArguments(document),
+                    );
+                  },
+                ),
+                contentPadding: EdgeInsets.only(left: 16.0),
+              ),
             );
           }).toList(),
         );
       },
     );
+  }
+
+  bool _isRecordedOn(DateTime date, DocumentSnapshot document) {
+    final List<dynamic> recordList = document['recordList'];
+    return recordList.any((record) {
+      final Map<String, dynamic> recordDate = record['recordDate'];
+      final bool isSameDate = (recordDate['year'] == date.year &&
+          recordDate['month'] == date.month &&
+          recordDate['date'] == date.day);
+      return isSameDate;
+    });
   }
 }
